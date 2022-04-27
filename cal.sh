346f9b9966be6bb25f2f0ca4e35a9f0da20bf4a7
Falguni Vashishtha <shrutisharma2302@gmail.com>
2022-04-27 12:09:50 +0530

    fixed

--- cal.sh
+++ cal.sh
@@ -1,14 +1,17 @@
+
 #! /bin/bash -x
 
 echo "1. Add"
 echo "2. Sub"
 echo "3. Mul"
 echo "4. Div"
+read a
+read b
 read calc
 case $calc in
-	1)echo "sum $((a + b))" ;;
-	2)echo "subtract $((a - b))" ;;
-	3)echo "multiply $((a * b))" ;;
-	4)echo "divide $((a / b))" ;;
-	*)echo "enter valid operation"
+	1 ) echo "sum $((a + b))" ;;
+	2 ) echo "subtract $((a - b))" ;;
+	3 ) echo "multiply $((a * b))" ;;
+	4 ) echo "divide $((a / b))" ;;
+	* ) echo "enter valid operation"
 esac
